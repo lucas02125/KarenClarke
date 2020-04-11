96339b1d1a0ef47869f49d233d0c0f4bcaa7285d
lucas02125 <63424962+lucas02125@users.noreply.github.com>
2020-04-11 13:50:48 +0100

    Update DataResults.sql

--- DataResults.sql
+++ DataResults.sql
@@ -8,13 +8,16 @@ REPLACE(hh.hh_Name,' ','') as 'Name of Hurricane' -- Values coming back from thi
 ,hh.hh_Date as 'Date of Landfall' --No specification of time as this is just required for time
 ,hd.hd_windspeed as 'Max Wind Speed'
 FROM HurricaneHeader hh 
-INNER JOIN HurricaneData hd ON hh.hh_Value = hd.hd_hh_Value
-WHERE hd.hd_date > '1900-01-01'
-AND hd.hd_status = 'HU'
+INNER JOIN HurricaneData hd ON hh.hh_Value = hd.hd_hh_Value --Inner join with header data and row data to join data of wind speed and date/Name
+WHERE hd.hd_date > '1900-01-01' --Since 1900
+AND hd.hd_status = 'HU' --Ensuring status is a Hurricane
 AND hd.hd_latitude BETWEEN @startpointLat AND @endpointLat
 AND hd.hd_longitude BETWEEN @startpointLong AND @endpointLong
---AND hd.hd_identifier = 'L'
-
+/* For the longitude and latitude I had ideas of taking in as geography data type but converting seemed a bit too tricky
+I used a startpoint and enpoint of latitude and longitude for the area of Florida and used the between clause to have an general data return 
+of areas the storm could hit in the Florida region
+AND hd.hd_identifier = 'L' As suggested in email, not to use Landfall identifier as 
+*/
 
 
 
